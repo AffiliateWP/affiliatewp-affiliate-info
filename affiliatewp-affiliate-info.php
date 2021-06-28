6b6e710eba16a3940f3d33c1fafcd5d589671069
Drew Jaynes <drew@affiliatewp.com>
2021-06-28 01:38:27 -0600

    Recreate main plugin file, swap in the main plugin header. #30

---
+++ affiliatewp-affiliate-info.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Plugin Name: AffiliateWP - Affiliate Info
+ * Plugin URI: https://affiliatewp.com/add-ons/official-free/affiliate-info/
+ * Description: Show information based on the affiliate's referral URL
+ * Author: Sandhills Development, LLC
+ * Author URI: https://sandhillsdev.com
+ * Version: 1.0.5
+ * Text Domain: affiliatewp-affiliate-info
+ *
+ * AffiliateWP is distributed under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * AffiliateWP is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AffiliateWP. If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package AffiliateWP Affiliate Info
+ * @category Core
+ * @version 1.0.5
+ */
+
